28e4917c96a974eff8494cfad67b6897d1e1d4fc
Ethan <thewhiterat2012@outlook.com>
2025-09-08 18:27:32 +0100

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-
-// Currently handles fade-in on load for all elements with 'fade-in'
-document.addEventListener('DOMContentLoaded', () => {
-    const fadeEls = document.querySelectorAll('.fade-in');
-    fadeEls.forEach(el => { el.style.opacity = '1'; });
-});
